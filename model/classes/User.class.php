62d77c6be46d214955e327960030d1d956821b9b
Simon Taeter <staeter@e-r9-p3.s19.be>
2019-10-10 18:46:13 +0200

    Changes to User class related to the supression of Session fro the architecture.

--- model/classes/User.class.php
+++ model/classes/User.class.php
@@ -5,7 +5,7 @@
 		private $_id;
 		private $_pseudo;
 		private $_email;
-		private $_logged;
+		private $_logged; //ni: other organisation for logged-in management
 		private $_db;
 
 		/*
@@ -23,12 +23,7 @@
 			}
 		}
 
-		private static function hash_password($password)
-		{
-			return $password; //ni: need real hash
-		}
-
-		private function __construct2($id, $db)
+		/*private function __construct2($id, $db)
 		{
 			// test parameters validity
 			if (!__CLASS__::is_valid_id($id)) {
@@ -40,9 +35,8 @@
 
 			// query from database
 			$query = "SELECT pseudo, email FROM user WHERE id_user = :id;";
-			if ($db->query($query, array(':id' => $id))) {
-				$row = $db->fetch();
-			}
+			$db->query($query, array(':id' => $id));
+			$row = $db->fetch();
 
 			// set object properties
 			$this->_id = $id;
@@ -51,6 +45,10 @@
 			$this->_logged = FALSE;
 			$this->_db = $db;
 		}
+		private static function is_valid_id($id)
+		{
+			return preg_match("/^[1-9][0-9]*$/", $id) ? TRUE : FALSE;
+		}*/
 
 		private function __construct3($email, $password, $db)
 		{ //ni: cookie management
@@ -70,10 +68,9 @@
 
 			// query from database
 			$query = 'SELECT id_user, pseudo FROM user WHERE email = :em AND password = :pw;';
-			if ($db->query($query, array(':em' => $email, ':pw' => $password))) {
-				$row = $db->fetch();
-			}
-			if (!isset($row)) {
+			$db->query($query, array(':em' => $email, ':pw' => $password));
+			$row = $db->fetch();
+			if ($row === false) {
 				throw new InvalidParamException("Failed constructing " . __CLASS__ . ". User-password combination not found in database.\n", 30);
 			}
 
@@ -106,14 +103,10 @@
 
 			// make sure email isn't in use
 			$query = 'SELECT id_user FROM user WHERE email = :em;';
-			if ($db->query($query, array(':em' => $email))) {
-				$row = $db->fetch();
-			}
-			if (!isset($row)) {
-				throw new InvalidParamException("Failed constructing " . __CLASS__ . ". Query failed.\n", );
-			}
-			if (array_key_exists('id_user', $row)) {
-				throw new InvalidParamException("Failed constructing " . __CLASS__ . ". Email alredy in use.\n", 42);
+			$db->query($query, array(':em' => $email));
+			$row = $db->fetch();
+			if ($row !== false) {
+				throw new InvalidParamException("Failed constructing " . __CLASS__ . ". Email alredy in use.\n", 45);
 			}
 
 			// hashing $password
@@ -121,11 +114,10 @@
 
 			// adding new user to database and pull the id_user
 			$query = 'INSERT INTO user (pseudo, email, password) VALUES (:ps, :em, :pw;); SELECT LAST_INSERT_ID() AS `id_user`;';
-			if ($db->query($query, array(':ps' => $pseudo, ':em' => $email, ':pw' => $password))) {
-				$row = $db->fetch();
-			}
-			if (!isset($row)) {
-				throw new InvalidParamException("Failed constructing " . __CLASS__ . ".\n", 1);
+			$db->query($query, array(':ps' => $pseudo, ':em' => $email, ':pw' => $password));
+			$row = $db->fetch();
+			if ($row === false) {
+				throw new DatabaseException("Failed constructing " . __CLASS__ . ". Id not pulled from db.\n");
 			}
 
 			// set object properties
@@ -134,13 +126,16 @@
 			$this->_email = $email;
 			$this->_logged = TRUE;
 			$this->_db = $db;
+
+			// send verification mail
+			$this->send_account_verification_request();
 		}
 
 
 		/*
 		** -------------------- Magic methods --------------------
 		*/
-		public function __destruct()
+		public function __destruct();
 		public function __toString()
 		{
 			return get_pseudo();
@@ -150,38 +145,117 @@
 		/*
 		** -------------------- Account verification --------------------
 		*/
-		public function send_account_verification_request()
-		public function receive_account_verification_request($unique_key)
+		public function send_account_verification_request();
+		public function receive_account_verification_request($unique_key);
 
 
 		/*
-		** -------------------- Set and get --------------------
+		** -------------------- Set --------------------
 		*/
 		public function set_pseudo($new)
+		{
+			if (!__CLASS__::is_valid_pseudo($new)) {
+				throw new InvalidParamException("Failed setting pseudo. Invalid pseudo.\n");
+			}
+
+			$query = 'UPDATE user SET pseudo = :ps WHERE id_user = :id;';
+			$modified_row_count = $db->exec($query, array(':ps' => $new, ':id' => $this->_id));
+			if ($modified_row_count !== 1) {
+				throw new DatabaseException("Fail setting pseudo. " . $modified_row_count . " rows have been modified in the database.\n");
+			}
+
+			$this->_pseudo = $new;
+		}
 		public function set_email($new)
-		public function set_password($old, $new, $confirm)
-		public function set_pref_mail_notification()
+		{
+			if (!__CLASS__::is_valid_email($email)) {
+				throw new InvalidParamException("Fail setting email. Invalid email.\n", 42);
+			}
 
-		public function get_id()
-		public function get_pseudo()
-		public function get_email()
-		public function get_pref_mail_notification()
+			$query = 'UPDATE user SET email = :em WHERE id_user = :id;';
+			if (($modified_row_count = $db->exec($query, array(':em' => $new, ':id' => $this->_id))) !== 1) {
+				throw new DatabaseException("Fail setting email. " . $modified_row_count . " rows have been modified in the database.\n");
+			}
 
+			$this->_email = $new;
+		}
+		public function set_password($old, $new)
+		{
+			if (!__CLASS__::is_valid_password($old)) {
+				throw new InvalidParamException("Fail setting password. Invalid old password.\n", 1);
+			}
+			if (!__CLASS__::is_valid_password($new)) {
+				throw new InvalidParamException("Fail setting password. Invalid new password.\n", 2);
+			}
+
+			// hashing pw
+			$old = __CLASS__::hash_password($old);
+			$new = __CLASS__::hash_password($new);
+
+			// pulling old pw from db and testing if it is the same as param
+			$query = 'SELECT password FROM user WHERE id_user = :id;';
+			$db->query($query, array(':id' => $email));
+			$row = $db->fetch();
+			if ($row === false) {
+				throw new DatabaseException("Fail setting password. `id_user` not found in database.\n");
+			}
+			if (strcmp($row['password'], $old) !== 0) {
+				throw new InvalidParamException("Fail setting password. Wrong old password.\n", 1);
+			}
+
+			// update db
+			$query = 'UPDATE user SET password = :pw WHERE id_user = :id;';
+			if (($modified_row_count = $db->exec($query, array(':pw' => $new, ':id' => $this->_id))) !== 1) {
+				throw new DatabaseException("Fail setting password. " . $modified_row_count . " rows have been modified in the database.\n");
+			}
+		}
+		public function set_pref_mail_notification() //ni
 
 		/*
-		** -------------------- Tools --------------------
+		** -------------------- Get --------------------
 		*/
-		public function send_mail($content)
+		public function get_pseudo()
+		{
+			return $this->_pseudo;
+		}
+		public function get_email()
+		{
+			return $this->_email;
+		}
+		public function get_pref_mail_notification() //ni
+
+		/*
+		** -------------------- Is valid --------------------
+		*/
 		public static function is_valid_email($email)
 		{
 			$patern = "/\A(?=[a-z0-9@.!#$%&'*+\/=?^_`{|}~-]{6,254}\z)(?=[a-z0-9.!#$%&'*+\/=?^_`{|}~-]{1,64}@)[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\z/"
 			return preg_match($patern, $email) ? TRUE : FALSE;
 		}
 		public static function is_valid_pseudo($pseudo)
-		public static function is_valid_password($password)
-		public static function is_valid_id($id)
 		{
-			return preg_match("/^[1-9][0-9]*$/", $id);
+			$patern = "/^[a-zA-Z0-9]{6,64}$/";
+			return preg_match($patern, $pseudo) ? TRUE : FALSE;
 		}
+		public static function is_valid_password($password)
+		{
+			$patern = "/(?s)^.{8,128}$/";
+			return preg_match($patern, $pseudo) ? TRUE : FALSE;
+		}
+
+		/*
+		** -------------------- Tools --------------------
+		*/
+		public function send_mail($content)
+		private static function hash_password($password)
+		{
+			return $password; //ni: need real hash
+		}
+
+		/*
+		** -------------------- Activities --------------------
+		*/
+		public function like($picture)
+		public function comment($picture, $content, $respond_to = null)
 	}
 ?>
