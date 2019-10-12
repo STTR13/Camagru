2f3d229adea4862f7538dc947165c99fe11cfab4
Simon Taeter <staeter@w-r9-p5.s19.be>
2019-10-12 15:07:18 +0200

    adding testing data to dumb_data for cookie

--- model/testing/dumb_data.sql
+++ model/testing/dumb_data.sql
@@ -5,3 +5,13 @@ VALUES
 	('John', 'john@mail.com', 'johnpw'),
 	('Clara', 'clara@insto.com', 'clarapw'),
 	('JohnD', 'john.doe@mail.com', 'johnpw');
+
+INSERT INTO `cookie`
+	(`id_user`)
+VALUES
+	(1),
+	(3),
+	(3),
+	(2),
+	(4),
+	(1);
