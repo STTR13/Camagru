b7a46629ad2fa3b626534d12eb395caa68ee67a4
Simon Taeter <staeter@w-r9-p5.s19.be>
2019-10-13 14:05:22 +0200

    User.class.test first implementation

---
+++ model/testing/User.class.testing.php
@@ -0,0 +1,19 @@
+<?php
+	require_once 'initialise.tests.php';
+	require_once '../classes/User.class.php';
+	require_once '../functions/hash_password.php';
+
+
+	/*
+	** --- valid construct ---
+	*/
+
+	// construct based on id_cookie
+	$u1 = new User(2, $db);
+
+	// construct on loging in
+	$u2 = new User("john@mail.com", hash_password("johnpw"), $db);
+
+	// construct on account creation
+	$u3 = new User("Joney", "charlie.bit@my.finger.us", hash_password("LEEEROY_JENKINS!"), $db);
+?>
