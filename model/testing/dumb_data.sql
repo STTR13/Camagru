5ea4371f79a43fb97da2edc2c31a9346ca7a79a7
Simon Taeter <staeter@w-r9-p5.s19.be>
2019-10-13 14:04:50 +0200

    hash paswords

--- model/testing/dumb_data.sql
+++ model/testing/dumb_data.sql
@@ -1,10 +1,10 @@
 INSERT INTO `user`
 	(`pseudo`, `email`, `password`)
 VALUES
-	('admin', 'admin@insto.com', 'adminpw'),
-	('John', 'john@mail.com', 'johnpw'),
-	('Clara', 'clara@insto.com', 'clarapw'),
-	('JohnD', 'john.doe@mail.com', 'johnpw');
+	('admin', 'admin@insto.com', "864194db6e6a3eaf9c95d11359ed727e866d29cd7a3e634a8e8c45ddd128f4c223af34fccc3d99e2aa669448afc20341c49a1a4930fb9696803d4558c20bca01"/* = 'adminpw'*/),
+	('John', 'john@mail.com', "4b0e76e863cfd20a0c5a11c15830d206772c4e93719ef429bfb073602b030da6ab5c533f270b20808f2059798d7ead9fdff3af441872ebb7b11cdac13f9ffa8c"/* = 'johnpw'*/),
+	('Clara', 'clara@insto.com', "e7115e4b4075fb4028c6b08efe570ba01ef0bbb00554fa3a00b25d812e64b242fb9f1bd668d1448fbe3dfa0ca8c168b0ca074049b8ec1c5ec35fddcfc1a49325"/* = 'clarapw'*/),
+	('JohnD', 'john.doe@mail.com', "4b0e76e863cfd20a0c5a11c15830d206772c4e93719ef429bfb073602b030da6ab5c533f270b20808f2059798d7ead9fdff3af441872ebb7b11cdac13f9ffa8c"/* = 'johnpw'*/);
 
 INSERT INTO `cookie`
 	(`id_user`)
