a331111efe091035644d5dea1150bc98f42d1927
Simon Taeter <staeter@e-r8-p6.s19.be>
2019-10-25 15:44:54 +0200

    post and testing

---
+++ view/galery/post.php
@@ -0,0 +1,29 @@
+<?php
+	require_once "../../model/classes/Picture.class.php";
+
+	function post(Picture $p) {
+		if (!Picture::is_valid($p)) {
+			throw new InvalidParamException("Failed running " . __FUNCTION__ . ". Invalid picture.\n", 1);
+		}
+
+?><div class="post">
+	<div class="head-info">
+		<user><?=$p->get_user_pseudo()?></user>
+		<date><?=$p->get_date()?></date>
+	</div>
+	<img class="picture" src="<?='/data/content/' . $p->get_path()?>">
+	<div class="foot-info">
+		<div class="like">
+			<button class="button" style="background-color: red;" type="button" name="like">
+				<img class="like-img" src="/data/images/white-heart.png">
+			</button>
+			<div class="like-amount">13 likes</div>
+		</div>
+	</div>
+</div><?php //ni: put the right amount of likes and add comments bellow
+	}
+
+	require_once '../../model/testing/initialise.tests.php';
+	$p = new Picture(2, $db); //t
+	post($p);
+?>
