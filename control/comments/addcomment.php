8c66e65486d619df860f6e54cac9c7a3756e9b73
Simon Taeter <staeter@e-r5-p2.s19.be>
2019-11-27 17:06:59 +0100

    mail on comment

--- control/comments/addcomment.php
+++ control/comments/addcomment.php
@@ -3,10 +3,18 @@
 	session_start();
 	require_once $_SERVER["DOCUMENT_ROOT"] . '/model/classes/Picture.class.php';
 	require_once $_SERVER["DOCUMENT_ROOT"] . '/view/comments/comment.php';
+	require_once $_SERVER["DOCUMENT_ROOT"] . '/model/functions/send_mail.php';
 
 	try {
 		$picture = new Picture($_POST['picture_id'], unserialize($_SESSION['db']));
 		$picture->add_comment(unserialize($_SESSION['user']), $_POST['content']);
+
+		send_mail(
+			$picture->get_user_id(),
+			unserialize($_SESSION['db']),
+			"INSTO: " . unserialize($_SESSION['user'])->get_pseudo() . " commented one of your pictures.",
+			$_POST['content']
+		);
 	} catch (Exception $e) {
 
 	} finally {
